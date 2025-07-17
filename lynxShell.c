90dea99ab565cc03c400db53ebeda319841c3cfb
Eric Leitsch <eric.leitsch@42-bit.de>
2025-07-17 08:24:00 +0200

    Ich hab noch ein paar Kommentare hinzugefügt falls du es doch vorstellen müsstest

--- lynxShell.c
+++ lynxShell.c
@@ -8,31 +8,42 @@
 
 #define BEFEHLSZEILE_LAENGE 512
 #define VERZEICHNIS_LAENGE 512
+#define DEBUG 0
 
 char letzterRückgabewert = 0;
 
 /* Shell von Sophia Weber - beinhaltet die gewünschten Funktionalitäten
    wie z. B. die Anzeige der Pfade, die Nutzung klassicher Befehle, Pipe, Semikolon usw. */
 
-void bearbeiteInput(char* input, char** inputArgs) {
+// Teile die Eingabe in Befehl und Argumente auf
+// Beispiel ls --all
+// befehl1 = ls
+// argv1 = [ls, -all]
+void bearbeiteEingabe(char* eingabe, char** eingabeArgumente) {
     int i = 0;
-    char* inputStart = input;
-    while (*input == ' ') {
-        input++;
+    char* eingabeStart = eingabe;
+
+    // lösche leer Zeichen am Start des Strings
+    while (*eingabe == ' ') {
+        eingabe++;
     }
-    memcpy(inputStart, input, strlen(input)+1);        
-    char *wort = strtok(input, " ");
+    // kopiere Eingabe um die übergebenen Daten nicht zu verändern
+    memcpy(eingabeStart, eingabe, strlen(eingabe)+1);
+    // Teile Eingabe in mehrere Argumente       
+    char *wort = strtok(eingabe, " ");
     while (wort && i < 14)
     {
-        inputArgs[i++] = wort;
-        // printf("|%s|", wort);
+        eingabeArgumente[i++] = wort;
+#if DEBUG == 1
+        printf("|%s|", wort);
+#endif
         wort = strtok(NULL, " ");
     }
-    // printf("\n");
-    inputArgs[i] = NULL;
+    eingabeArgumente[i] = NULL;
     
 }
 
+// Verarbeite eine Anforderung das Verzeichnis zu wechseln
 // cd test
 // cd ~/test
 // cd /home/bla/blub
@@ -45,7 +56,9 @@ bool verarbeiteCD(char *befehl)
     {
         return false;
     }
-    pfad = strtok(NULL, " "); // Bei Null wird der Pointer vom vorherigen strtok genommen an der stelle wo das " " ersetzt wurde
+
+    // Bei Null wird der Pointer vom vorherigen strtok genommen an der stelle wo das " " ersetzt wurde
+    pfad = strtok(NULL, " "); 
     if (pfad == NULL)
     {
         return false;
@@ -71,10 +84,14 @@ bool verarbeiteCD(char *befehl)
     {
         strcpy(absoluterPfad, pfad);
     }
-    // printf("Ändere Ordner: %s\n", absoluterPfad);
+    #if DEBUG == 1
+    printf("Ändere Ordner: %s\n", absoluterPfad);
+    #endif
+    // Ändere Konsolenpfad
     chdir(absoluterPfad);
 }
 
+// Verarbeite einen Befehl mit einer Pipe im String
 void verarbeitePipe(char *befehl)
 {
     char *argv1[15] = {0};
@@ -82,49 +99,57 @@ void verarbeitePipe(char *befehl)
     char *befehl1 = strtok(befehl,"|");
     char *befehl2 = strtok(NULL,"|");
 
-    bearbeiteInput(befehl1, argv1);
-    bearbeiteInput(befehl2, argv2);
+    // Teile die Eingabe in Befehl und Argumente auf
+    bearbeiteEingabe(befehl1, argv1);
+    bearbeiteEingabe(befehl2, argv2);
 
+    // öffne pipe
     int pipefd[2];
-    pipe(pipefd);
+    pipe(pipefd); 
 
-    pid_t pid1 = fork();
+    // erstelle Kind 1 Prozess
+    pid_t pid1 = fork(); 
     if (pid1 == 0) {
-        // Kind 1: false
+        // Kind 1
         close(pipefd[0]);                 // liest nicht
         dup2(pipefd[1], STDOUT_FILENO);  // stdout → pipe
         close(pipefd[1]);
         execvp(befehl1, argv1);
         perror("Fehler im Argument 1");
-        exit(1);
+        exit(EXIT_FAILURE);                        // Ende Kind 1 Prozess
     }
 
-    pid_t pid2 = fork();
+    // erstelle Kind 2 Prozess
+    pid_t pid2 = fork(); 
     if (pid2 == 0) {
-        // Kind 2: wc
+        // Kind 2
         close(pipefd[1]);                // schreibt nicht
         dup2(pipefd[0], STDIN_FILENO);  // stdin ← pipe
         close(pipefd[0]);
         execvp(befehl2, argv2);
         perror("Fehler im Argument 2");
-        exit(1);
+        exit(EXIT_FAILURE);                        // Ende Kind 2 Prozess
     }
-    // Elternprozess: beide Seiten schließen
+    // beide Pipes im Elternprozess schließen
     close(pipefd[0]);
     close(pipefd[1]);
 
-    // Warten auf beide Kinder
+    // Warten auf beide Kinder im Elternprozess
     int status1, status2;
     waitpid(pid1, &status1, 0);
     waitpid(pid2, &status2, 0);
     fflush(stdout);
-    
-    // printf("%s Exit-Code: %d\n", befehl1, WEXITSTATUS(status1));
-    // printf("%s Exit-Code: %d\n", befehl2,  WEXITSTATUS(status2));
+
+    #if DEBUG == 1
+    printf("%s Exit-Code: %d\n", befehl1, WEXITSTATUS(status1));
+    printf("%s Exit-Code: %d\n", befehl2,  WEXITSTATUS(status2));
+    #endif
+    letzterRückgabewert = WEXITSTATUS(status2);
 }
 
 void verarbeiteEinzelBefehl(char *befehl, char **arg)
-{
+{   
+    // erstelle Kind Prozess
     pid_t pid = fork();
     if (pid == -1)
     {
@@ -132,12 +157,14 @@ void verarbeiteEinzelBefehl(char *befehl, char **arg)
         return;
     }
 
+    // Bereich des Kind Prozesses
     if (pid == 0)
     {
         execvp(befehl, arg);
         perror("Fehler beim Ausführen des Befehls");
         exit(EXIT_FAILURE);
     }
+    // Bereich des Eltern Prozesses
     else
     {
         int status;
@@ -151,6 +178,7 @@ void verarbeiteSighup(int signum) {
     printf("%i\n",letzterRückgabewert);
 }
 
+// Zeige momentanen Pfad der Konsole an
 void zeigePfad(char *verzeichnis)
 {
     // Quelle für Arbeitsverzeichnis anzeigen: https://stackoverflow.com/questions/298510/how-to-get-the-current-directory-in-a-c-program
@@ -164,30 +192,36 @@ void zeigePfad(char *verzeichnis)
     fflush(stdout);
 }
 
-bool holeInput(char *befehlZeile, size_t laenge)
+// hole eine neue Eingabe vom Nutzer
+bool holeEingabe(char *befehlZeile, size_t laenge)
 {
     if (fgets(befehlZeile, laenge, stdin) == NULL)
     {
-        printf("Fehler bei der Eingabe.\n");
-        fflush(stdin);
-        exit(0);
+        // Wenn etwas falsches oder EOF eingegeben wird
+        perror("Fehler bei der Eingabe.\n");
+        exit(EXIT_SUCCESS);
     }
 
-    // printf("Die Eingabe war: %s", befehlZeile);
+    #if DEBUG == 1
+    printf("Die Eingabe war: %s", befehlZeile);
+    #endif
 
     size_t len = strlen(befehlZeile);
+    // Ersetze \n durch ein string ende
     if (len > 0 && befehlZeile[len - 1] == '\n')
         befehlZeile[len - 1] = '\0';
 
+    // schließe das Programm
     if (strcmp(befehlZeile, "exit") == 0)
-        exit(0);
+        exit(EXIT_SUCCESS);
 
+    // gebe letzten Rückgabewert des Kind Prozesses zurück
     if (strcmp(befehlZeile, "ret") == 0) {
         printf("%i\n",letzterRückgabewert);
         return false;
     }
         
-
+    // kein String eingegeben
     if (befehlZeile[0] == '\0')
     {
         return false;
@@ -196,13 +230,13 @@ bool holeInput(char *befehlZeile, size_t laenge)
     return true;
 }
 
-void verarbeiteInput(char *befehlZeile)
+// Verarbeite die Eingabe vom Nutzer
+void verarbeiteEingabe(char *befehlZeile)
 {
-    // toDo einzelne funktionen wie trennung ; pipe usw in einzelne Funktionen trennen
-    // die können dann wiederverwendet werden ;-)
     char *befehlTeil = strtok(befehlZeile, ";");
     char befehl[BEFEHLSZEILE_LAENGE] = {0};
 
+    // Solange ein Abschnitt existiert, wird die while Schleife ausgeführt
     while (befehlTeil != NULL)
     {
         befehlZeile+=strlen(befehlTeil)+1;
@@ -210,21 +244,26 @@ void verarbeiteInput(char *befehlZeile)
             befehlTeil++;
         memcpy(befehl, befehlTeil, strlen(befehlTeil)+1);
 
+        // Verarbeite einen Befehl mit einer Pipe im String
         if (strchr(befehl, '|'))
         {
             verarbeitePipe(befehl);
         }
+        // Verarbeite eine Anforderung das Verzeichnis zu wechseln
         else if (strncmp(befehl, "cd", 2) == 0)
         {
             verarbeiteCD(befehl);
         }
+        // Verarbeite alle sonstigen Befehle, welche in PATH bekannt sind oder manuell aufgerufen werden
         else
         {
             char *befehlAufgeteilt[15];
-            bearbeiteInput(befehl, befehlAufgeteilt);
+            // Teile die Eingabe in Befehl und Argumente auf
+            bearbeiteEingabe(befehl, befehlAufgeteilt);
             verarbeiteEinzelBefehl(befehl, befehlAufgeteilt);
         }
 
+        // Trenne Eingabe um eventuell den nächsten Abschitt zu bearbeiten
         befehlTeil = strtok(befehlZeile, ";");
     }
 }
@@ -234,8 +273,6 @@ int main()
     char verzeichnis[VERZEICHNIS_LAENGE];
     char befehlszeile[BEFEHLSZEILE_LAENGE];
     char data[BEFEHLSZEILE_LAENGE] = "date +%s |wc -c";
-    // verarbeiteInput(data);
-    // return;
 
     // Signal-Handler registrieren
     if (signal(SIGHUP, verarbeiteSighup) == SIG_ERR) {
@@ -244,13 +281,16 @@ int main()
     }
     while (1)
     {
-        char verzeichnis[VERZEICHNIS_LAENGE];
-        char befehlszeile[BEFEHLSZEILE_LAENGE];
+        char verzeichnis[VERZEICHNIS_LAENGE] = {0};
+        char befehlszeile[BEFEHLSZEILE_LAENGE] = {0};
 
+        // Zeige momentanen Pfad der Konsole an
         zeigePfad(verzeichnis);
-        if (holeInput(befehlszeile, sizeof(befehlszeile)))
+        // hole eine neue Eingabe vom Nutzer
+        if (holeEingabe(befehlszeile, sizeof(befehlszeile)))
         {
-            verarbeiteInput(befehlszeile);
+            // Verarbeite die Eingabe vom Nutzer
+            verarbeiteEingabe(befehlszeile);
         }
     }
 }
